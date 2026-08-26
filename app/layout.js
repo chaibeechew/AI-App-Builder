4b3ac234e22ea5e5244d27003af47adda48f7ff0
chaibeechew <chaibeechew@gmail.com>
2026-08-26 22:31:57 +0800

    Add account navigation to app shell

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import AccountNav from "./components/AccountNav";
 
 export const metadata = {
   title: "AI App Builder",
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AccountNav />
+        {children}
+      </body>
     </html>
   );
 }
