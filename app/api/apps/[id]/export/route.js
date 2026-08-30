8e28bf102cff0a9d43ad49b5b83ae73a5fbe748f
chaibeechew <chaibeechew@gmail.com>
2026-08-30 22:53:00 +0800

    Fix project export import path

--- app/api/apps/[id]/export/route.js
+++ app/api/apps/[id]/export/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { createClient } from "../../../../../../lib/supabase/server.js";
+import { createClient } from "../../../../../lib/supabase/server.js";
 
 export async function GET(_request,{params}){
   try{
