80e2d8627a51b193fdb309d83d6304b50810a13f
chaibeechew <chaibeechew@gmail.com>
2026-08-31 21:54:12 +0800

    Mount publishing readiness assistant

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ import AdaptiveWallpaperEngine from "./components/AdaptiveWallpaperEngine";
 import PremiumJourneyTheme from "./components/PremiumJourneyTheme";
 import PreciseEditAssistant from "./components/PreciseEditAssistant";
 import GeneratedDataManager from "./components/GeneratedDataManager";
+import PublishingReadinessAssistant from "./components/PublishingReadinessAssistant";
 import { PRODUCT_BRAND } from "../lib/ai/premium-visual-policy.js";
 
 export const metadata = {
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
         {children}
         <PreciseEditAssistant />
         <GeneratedDataManager />
+        <PublishingReadinessAssistant />
         <StudioLauncher />
         <ReferenceUploader />
         <SoolenVoiceAssistant />
