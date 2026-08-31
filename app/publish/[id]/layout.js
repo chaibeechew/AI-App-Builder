fc9200cb1b5f52c0f71f398c527c3e00084a60b6
chaibeechew <chaibeechew@gmail.com>
2026-08-31 21:57:52 +0800

    Remove extra dynamic publishing function

--- app/publish/[id]/layout.js
+++
@@ -1,6 +0,0 @@
-import PublishingReadinessPanel from "./PublishingReadinessPanel";
-
-export default async function PublishLayout({children,params}){
-  const {id}=await params;
-  return <>{children}<PublishingReadinessPanel appId={id}/></>;
-}
